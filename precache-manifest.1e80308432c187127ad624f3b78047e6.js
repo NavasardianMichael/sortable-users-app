42e29dd546cd40dac3b0fb4cfd847761cdd3ddfa
Navasardian Michael <navasardianmichael@gmail.com>
2020-07-02 17:25:13 +0400

    Updates

---
+++ precache-manifest.1e80308432c187127ad624f3b78047e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94ba5453435f63e152fa623726d0008",
+    "url": "/sortable-users-app/index.html"
+  },
+  {
+    "revision": "b77a83d7188dc6aad0cd",
+    "url": "/sortable-users-app/static/css/2.24b43e67.chunk.css"
+  },
+  {
+    "revision": "b1a2a447a037579217e3",
+    "url": "/sortable-users-app/static/css/main.3d948f0e.chunk.css"
+  },
+  {
+    "revision": "b77a83d7188dc6aad0cd",
+    "url": "/sortable-users-app/static/js/2.30e9a07c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sortable-users-app/static/js/2.30e9a07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e174ca558fce4d666a84",
+    "url": "/sortable-users-app/static/js/3.b74ec16e.chunk.js"
+  },
+  {
+    "revision": "b1a2a447a037579217e3",
+    "url": "/sortable-users-app/static/js/main.748db332.chunk.js"
+  },
+  {
+    "revision": "7bd9cc1ee37b5f7102fd",
+    "url": "/sortable-users-app/static/js/runtime-main.36b184b9.js"
+  },
+  {
+    "revision": "5c58d0e1d2ec48e766846af99eaeb6f8",
+    "url": "/sortable-users-app/static/media/flags.5c58d0e1.png"
+  },
+  {
+    "revision": "27670c535824abb6ae7263d60fdc8a86",
+    "url": "/sortable-users-app/static/media/flags@2x.27670c53.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sortable-users-app/precache-manifest.1e80308432c187127ad624f3b78047e6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sortable-users-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
